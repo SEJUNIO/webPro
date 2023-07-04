b747a01448379ff157c7dac683ce8df93785f130
SEJUNIO <layy537@kakao.com>
2023-07-04 15:16:59 +0900

    five

--- source/01_java/ch08_class2/src/com/lec/ex0_janmuri/PersonTestMain.java
+++ source/01_java/ch08_class2/src/com/lec/ex0_janmuri/PersonTestMain.java
@@ -1,5 +1,12 @@
 package com.lec.ex0_janmuri;
-//클래스
+//클래스 // 자바파일에 public class 한개씩 구현
+//메인
+public class PersonTestMain {
+	public static void main(String[] args) {
+		PersonInfo person1 = new PersonInfo("홍길동", 22, 'm');
+		person1.print();
+	}
+}
 class PersonInfo {
 	private String name;
 	private int age;
@@ -13,10 +20,4 @@ class PersonInfo {
 		System.out.println("이름 : " + name + ", 나이 : " + age +", 성별 : " +gender);
 	}
 }
-//메인
-public class PersonTestMain {
-	public static void main(String[] args) {
-		PersonInfo person1 = new PersonInfo("홍길동", 22, 'm');
-		person1.print();
-	}
-}
+
--- source/01_java/ch08_class2/src/com/lec/ex3_product/Product.java
+++ source/01_java/ch08_class2/src/com/lec/ex3_product/Product.java
@@ -1,5 +1,21 @@
 package com.lec.ex3_product;
-
+// 데이터:serialNo
 public class Product {
-
+	private int serialNo;
+	public static int count = 100; // 초기값  //static이 있으면 공유한다.
+	public Product( ) {   //생성자함수
+		serialNo = ++count;
+	}
+	public static void staticMethod(){
+		System.out.println("static 메소드");
+	}
+	public void infoPrint() {
+		System.out.println("시리얼번호 : " + serialNo + "\t공유변수 count : " + count);
+	}
+	public int getSerialNo() {
+		return serialNo;
+	}
+	public void setSerialNo(int serialNo) {
+		this.serialNo = serialNo;
+	}
 }
