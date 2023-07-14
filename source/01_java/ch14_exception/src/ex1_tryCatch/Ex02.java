285b5cadeb10f17f5f30cb481b2c37478f0fc178
SEJUNIO <layy537@kakao.com>
2023-07-14 09:50:17 +0900

    last

--- source/01_java/ch14_exception/src/ex1_tryCatch/Ex02.java
+++ source/01_java/ch14_exception/src/ex1_tryCatch/Ex02.java
@@ -2,16 +2,15 @@ package ex1_tryCatch;
 
 import java.util.Scanner;
 
-public class Ex01 {
+public class Ex02 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int i, j =1; //사용자에게 입력받을 수 저장
+		int i, j; //사용자에게 입력받을 수 저장
 		System.out.print("첫번째 정수는(사칙연산 결과를 위한)?");
 		i = scanner.nextInt();
 		System.out.print("두번째 정수는(사칙연산 결과를 위한)?");
-
+		j = scanner.nextInt();
 		try {
-			j = scanner.nextInt();
 			System.out.println("i / j = " + (i/j)); // 예외가 발생할 수도 있고, 안 할 수 있는 부분
 		}catch(ArithmeticException e) { //ArithmeticException 예외 객체 발생시 수행 부분
 			//e.printStackTrace(); //좀 더 자세한 예외 메세지
