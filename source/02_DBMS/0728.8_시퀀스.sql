80ebf628d29fe17b79bacca0c89feddf67331852
SEJUNIO <layy537@kakao.com>
2023-07-27 18:02:38 +0900

    DML

---
+++ source/02_DBMS/0728.8_시퀀스.sql
@@ -0,0 +1,11 @@
+-- [VIII] Sequence : 순차번호 생성기. 대부분 인위적인 PRIMARY KEY(PK) 사용 용도
+DROP SEQUENCE FRIEND_SEQ;
+CREATE SEQUENCE FRIEND_SEQ
+  START WITH 1 -- 1부터 시작 
+  INCREMENT BY 1 -- 1씩 증가
+  MAXVALUE 9999  -- 최대값
+  MINVALUE  1  -- 최소값
+  NOCACHE
+  NOCYCLE;
+  
+  -- NOCACHE, NOCYCLE, MAXVALUE 자주사용
